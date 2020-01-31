c1c7c54e06f5db3bb6888b25718b245cc0a93272
Luccas Avila <luccasavila62@gmail.com>
2020-01-31 13:47:38 -0300

    created Global Style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-function App() {
-  return <h1>Learn Kana</h1>;
-}
+const App = () => {
+  return (
+    <>
+      <h1>Learn Kana</h1>
+      <GlobalStyle />
+    </>
+  );
+};
 
 export default App;
---
+++ src/styles/global.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap');
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: 0;
+        font-family: 'Noto Sans', sans-serif;
+    }
+`;
