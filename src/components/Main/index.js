c1b0ac949b3c99ced9061e247595aa82eedd4152
Luccas Avila <luccasavila62@gmail.com>
2020-02-24 15:53:08 -0300

    Showing random kana

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Container, Kana, Text, KanaInput, Answer} from './styles';
-import kana from '../../utils/kana';
+import kanaList from '../../utils/kana';
 
 const Main = () => {
 
     // Return a random kana
     const _randomKana = () => {
-        const { hiragana } = kana;
+        const { hiragana } = kanaList;
 
         const syllabary = hiragana[Math.floor(Math.random() * hiragana.length)];
         const syllable = Object.keys(syllabary);
@@ -14,9 +14,11 @@ const Main = () => {
         return syllable[Math.floor(Math.random() * syllable.length)];
     }
 
+    const [kana, setKana] = useState(_randomKana());
+
     return(
         <Container>
-            <Kana>あ</Kana>
+            <Kana>{kana}</Kana>
             <Text>What is this letter in rõmaji?</Text>
             <KanaInput />
             <Answer correct><span>Correct!</span> <b>あ</b> is <b>a</b> on hiragana</Answer>
