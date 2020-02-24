36a6244708e6aaa0a7b636621e5fdfce211cb8c9
Luccas Avila <luccasavila62@gmail.com>
2020-02-24 15:49:58 -0300

    random Kana function created

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import {Container, Kana, Text, KanaInput, Answer} from './styles';
+import kana from '../../utils/kana';
 
 const Main = () => {
+
+    // Return a random kana
+    const _randomKana = () => {
+        const { hiragana } = kana;
+
+        const syllabary = hiragana[Math.floor(Math.random() * hiragana.length)];
+        const syllable = Object.keys(syllabary);
+
+        return syllable[Math.floor(Math.random() * syllable.length)];
+    }
+
     return(
         <Container>
             <Kana>あ</Kana>
