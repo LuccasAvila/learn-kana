58fcc8ebd5fd6043377eb18e4857c1485c24ce8f
Luccas Avila <luccasavila62@gmail.com>
2020-02-24 21:34:19 -0300

    Updates

---
+++ precache-manifest.ce2351f0aae7a0d97dee6fa082759c57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f637f8405a3685e04b7a3780961b2f95",
+    "url": "/learn-kana/index.html"
+  },
+  {
+    "revision": "d884bdd42f1e93d77e74",
+    "url": "/learn-kana/static/js/2.dcc09092.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/learn-kana/static/js/2.dcc09092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f932c9f18ed6edbef84",
+    "url": "/learn-kana/static/js/main.bca3f454.chunk.js"
+  },
+  {
+    "revision": "4be5bb7eb98f768bcacf",
+    "url": "/learn-kana/static/js/runtime-main.5393ac6b.js"
+  }
+]);
